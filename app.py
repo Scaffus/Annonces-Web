a9544aef4cb09540434db179b9aff37f621184e8
Scaffus <scaffus.scaffus@gmail.com>
2021-04-14 20:20:30 +0200

    v0.4

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
+import os
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///annonces.db'
+ALLOWED_EXTENSIONS = ["jpg", "jpeg"]
+app.config['MAX_CONTENT_PATH'] = 10000000
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['UPLOAD_FOLDER'] = "static/images/annonces"
 db = SQLAlchemy(app)
 
 class Post(db.Model):
@@ -16,13 +20,20 @@ class Post(db.Model):
     price           = db.Column(db.Text, nullable = False)
     username        = db.Column(db.Text, nullable = False)
     asset_condition = db.Column(db.Text, nullable = False)
+    date            = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
 
     def __repr__(self):
         return '<Annonce: {}>'.format(str(self.id))
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/')
+def index():
+            
+    allPosts = Post.query.order_by(Post.date.desc()).all()
+    return render_template('index.html', annonces=allPosts)
+
+@app.route('/annonce/create', methods=['GET', 'POST'])
+def all_post():
 
     if request.method == 'POST':
 
@@ -33,27 +44,59 @@ def home():
         price           = request.form['price']
         username        = request.form['username']
         asset_condition = request.form['assetCondition']
+        image           = request.files['image']
 
         # if title:
         if description and mailName and mailHost and title and description and price and asset_condition and username:
 
             mail = '{}@{}'.format(mailName, mailHost)
 
-            newPost = Post(title=title, description=description, mail=mail, price=price, username=username, asset_condition=asset_condition)
+            newPost = Post(title=title, description=description, mail=mail.lower(), price=price, username=username.lower(), asset_condition=asset_condition)
 
             db.session.add(newPost)
             db.session.commit()
 
+            if image:
+    
+                image.save('img_annonce_{}.png'.format(newPost.id))
         
-        return redirect('/')
+    return redirect('/')
+
+@app.route('/annonce/search', methods=['GET', 'POST'])
+def search_post():
+
+    if request.method == 'POST':
+
+        search         = request.form['search']
+        what_to_search = request.form['what_to_search']
         
-    else:
+        if search and what_to_search:
 
-        allPosts = Post.query.order_by(Post.id.desc()).all()
-        return render_template('index.html', annonces=allPosts)
+            if what_to_search == 'username':
+                allPosts = Post.query.filter_by(username=search).all()
 
-@app.route('/post/delete/<int:id>')
-def delete(id):
+            elif what_to_search == 'title':
+                allPosts = Post.query.filter_by(title=search).all()
+
+            elif what_to_search == 'price':
+                allPosts = Post.query.filter_by(price=search).all()
+            
+            elif what_to_search == 'mail':
+                allPosts = Post.query.filter_by(mail=search).all()
+
+            elif what_to_search == 'asset_condition':
+                allPosts = Post.query.filter_by(asset_condition=search).all()
+            
+            try:
+                return render_template('index.html', annonces=allPosts)
+            
+            except UnboundLocalError:
+                return render_template('index.html', error='Désolé, nous n\'avons rien trouver qui correspond à la recherche par {} \"{}\"'.format(what_to_search, search))
+
+    return redirect('/')
+
+@app.route('/annonce/delete/<int:id>')
+def delete_post(id):
 
     post = Post.query.get_or_404(id)
 
@@ -62,7 +105,7 @@ def delete(id):
 
     return redirect('/')
     
-@app.route('/newPost')
+@app.route('/annonce/new')
 def new_post():
 
     return render_template('newPost.html')
